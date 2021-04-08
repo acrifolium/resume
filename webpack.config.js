ab328ba3bfe79cc3bb2feaa38f5c09ac6c804538
acrifolium <olivier.azevedo@gmail.com>
2021-04-08 16:13:55 +0200

    First commit - webpack + bulma

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 9000,
+    },
+    module: {
+        rules: [{
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader'
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                sourceMap: true,
+                // options...
+                }
+            }
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: 'css/styles.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        }),
+    ],
+};
