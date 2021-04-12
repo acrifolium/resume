508ad600a9010c48defa6a10032b90b075399e70
acrifolium <olivier.azevedo@gmail.com>
2021-04-12 17:47:21 +0200

    add text for job experiences

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 require('./styles.scss');
 import acrifolium from './assets/images/acrifolium.jpg';
+import m3Groupe from './assets/images/m3-groupe.jpeg';
+import axa from './assets/images/axa.png';
+import sgib from './assets/images/sgib.png';
+import finexkap from './assets/images/finexkap.png';
+import vinci from './assets/images/vinci.png';
+import capgemini from './assets/images/capgemini.jpeg';
