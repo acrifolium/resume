f7c6a191ce37842a7234a2f015eb442362a1183c
acrifolium <olivier.azevedo@gmail.com>
2021-04-13 16:14:11 +0200

    add new sections references skills and courses

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import axa from './assets/images/axa.png';
 import sgib from './assets/images/sgib.png';
 import finexkap from './assets/images/finexkap.png';
 import vinci from './assets/images/vinci.png';
-import capgemini from './assets/images/capgemini.jpeg';
+import capgemini from './assets/images/capgemini.png';
