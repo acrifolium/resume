f9975ce854ca81b067354abbaadb334aa145c0c0
acrifolium <olivier.azevedo@gmail.com>
2022-02-08 13:04:28 +0100

    Add total energies experience

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # resume
 
+# Launch
+- yarn start
+- http://localhost:9000/
+
 In order to print this resume you need to use the print popup fron Google Chrome browser and customize it.
 
 Please :
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('./styles.scss');
 import acrifolium from './assets/images/acrifolium.jpg';
+import totalEnergies from './assets/images/total-energies.jpeg';
 import m3Groupe from './assets/images/m3-groupe.jpeg';
 import axa from './assets/images/axa.png';
 import sgib from './assets/images/sgib.png';
