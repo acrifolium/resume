b877ae9baf83ef25b7b9e2aa307021fe17ecca21
acrifolium <olivier.azevedo@gmail.com>
2022-09-06 13:08:46 +0200

    New sdv version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('./styles.scss');
 import acrifolium from './assets/images/acrifolium.jpg';
+import sdv from './assets/images/sdv.png';
 import totalEnergies from './assets/images/total-energies.jpeg';
 import m3Groupe from './assets/images/m3-groupe.jpeg';
 import axa from './assets/images/axa.png';
